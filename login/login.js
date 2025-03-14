39c9fcfe3e0ecf658b9f3d3d9c93e4a2094c8618
KyreMichael <kmshamwell@aggies.ncat.edu>
2025-03-14 11:36:49 -0400

    Trying to see updates for html

--- login/login.js
+++ login/login.js
@@ -1,13 +1,24 @@
-function openPopup(type) {
-    document.getElementById(type + 'Popup').style.display = 'block';
+/**
+ * Opens the specified popup and displays the overlay.
+ * @param {string} id - The ID of the popup to be opened.
+ */
+function openPopup(id) {
+    document.getElementById(id).style.display = 'block';
     document.getElementById('popupOverlay').style.display = 'block';
 }
 
-function closePopup(type) {
-    document.getElementById(type + 'Popup').style.display = 'none';
+/**
+ * Closes the specified popup and hides the overlay.
+ * @param {string} id - The ID of the popup to be closed.
+ */
+function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
     document.getElementById('popupOverlay').style.display = 'none';
 }
 
+/**
+ * Closes all popups and hides the overlay.
+ */
 function closeAllPopups() {
     document.getElementById('signinPopup').style.display = 'none';
     document.getElementById('signupPopup').style.display = 'none';
