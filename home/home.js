46d34cc0aee5071ce86bb659413f1e14701568c0
KyreMichael <kmshamwell@aggies.ncat.edu>
2025-03-26 18:06:41 -0400

    Working on signout

--- home/home.js
+++ home/home.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
             localStorage.removeItem("role");
 
             // ✅ Redirect to login page
-            window.location.href = "../login/index.html"; // ✅ Go up one directory, then into /login/
+            window.location.href = "/login/index.html"; // ✅ Go up one directory, then into /login/
         });
     }
 });
